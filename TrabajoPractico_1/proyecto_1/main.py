9b40ac9444538250f49a987990cdb01fd244cbcf
ValenEberhardt <valentin_eberhardt@hotmail.com>
2025-09-11 16:12:10 -0300

    actualizando main y listaDobleEnlazada

--- TrabajoPractico_1/proyecto_1/main.py
+++ TrabajoPractico_1/proyecto_1/main.py
@@ -1,9 +1,9 @@
-from modules.ListaDobleEnlazada import ListaDobleEnlazada
+from modules.ListaDobleEnlazada import ListaDobleEnlazada,Nodo
 from matplotlib import pyplot as plt
 from random import randint
 import time
 
-tamanos = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+"""tamanos = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
 
 tiempos_eliminar_milista = []
 tiempos_eliminar_list = []
@@ -49,8 +49,47 @@ plt.title('Comparación de tiempos de eliminación')
 plt.legend()
 plt.grid()
 plt.show()
+"""
 
+lista = ListaDobleEnlazada()
 
+def medir_tiempo(funcion, lista):
+    inicio = time.time()
+    funcion(lista)
+    fin = time.time()
+    return fin - inicio
+
+tam_listas = [11, 123, 544, 1386, 5753, 13798]
+tiempos_len = []
+tiempos_copiar = []
+tiempos_invertir = []
+
+for tam in tam_listas:
+    lista = ListaDobleEnlazada()
+    
+    # Agregar elementos a la lista
+    for i in range(tam):
+        lista.agregar_al_final(i)
+    
+    # Medir tiempo de len
+    tiempos_len.append(medir_tiempo(lambda x: len(x), lista))
+    
+    # Medir tiempo de copiar
+    tiempos_copiar.append(medir_tiempo(lambda x: x.copiar(), lista))
+    
+    # Medir tiempo de invertir
+    tiempos_invertir.append(medir_tiempo(lambda x: x.invertir(), lista))
+
+# Graficar los resultados
+plt.plot(tam_listas, tiempos_len, label='len', marker='o')
+plt.plot(tam_listas, tiempos_copiar, label='copiar', marker='o')
+plt.plot(tam_listas, tiempos_invertir, label='invertir', marker='o')
+plt.xlabel('Cantidad de elementos N')
+plt.ylabel('Tiempo de ejecución (s)')
+plt.title('Tiempo de ejecución de métodos len, copiar, invertir')
+plt.legend()
+plt.grid(True)
+plt.show()
 
 
 
