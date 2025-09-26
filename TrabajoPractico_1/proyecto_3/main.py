2af21803cfbb3e7619a454e52312e30dde65ce96
ValenEberhardt <valentin_eberhardt@hotmail.com>
2025-09-26 10:44:57 -0300

    Agrega doc

--- TrabajoPractico_1/proyecto_3/main.py
+++ TrabajoPractico_1/proyecto_3/main.py
@@ -2,34 +2,72 @@ from modules.ordenamiento_burbuja import ordenamiento_burbuja
 from modules.ordenamiento_quicksort import quicksort
 from modules.ordenamiento_radixsort import radix_sort
 from random import randint
+import time
+from matplotlib import pyplot as plt
 
 #Probamos que los algoritmos funcionen correctamente 
 
 #Lista de 700 numeros del 1 al 1000 desordenados
-numeros = []
+burbuja_time = []
+quicksort_time = []
+radixsort_time = []
+listasort_time = [] #Haciendo referencia al ordenamiento sort de las listas ya implementadas en python
 
-for i in range(700):
-    n = randint(1,1000)
-    numeros.append(n)
+tamanios = [1, 10, 100, 200, 500, 700, 1000]
 
-print("Lista desordenada " , numeros)
-
+plt.figure(figsize=(10, 6))
 
 #Ordenamiento burbuja
+
+for n in tamanios:
+    numeros = [randint(1, 10000) for _ in range(n)]
     
-burbuja_numeros = ordenamiento_burbuja(numeros)
-print("\nLista ordenada con Ordenamiento Burbuja " , burbuja_numeros)
+    inicio = time.perf_counter()
+    burbuja_numeros = ordenamiento_burbuja(numeros)
+    fin = time.perf_counter()
+    burbuja_time.append(fin - inicio)
+
 
 #Ordenamiento quicksort
 
-quicksort_numeros = quicksort(numeros)
-print("\nLista ordenada con Ordenamiento Quicksort " , quicksort_numeros)
+for n in tamanios:
+    numeros = [randint(1, 10000) for _ in range(n)]
+    
+    inicio = time.perf_counter()
+    quicksort_numeros = quicksort(numeros)
+    fin = time.perf_counter()
+    quicksort_time.append(fin - inicio)
 
-radixsort_numeros = radix_sort(numeros)
-print("\nLista ordenada con Ordenamiento Radixsort " , radixsort_numeros)
+
+#Ordenamiento RadixSort
+
+for n in tamanios:
+    numeros = [randint(1, 10000) for _ in range(n)]
+    
+    inicio = time.perf_counter()
+    radix_sort_numeros = radix_sort(numeros)
+    fin = time.perf_counter()
+    radixsort_time.append(fin - inicio)
 
 
-if burbuja_numeros == quicksort_numeros and radixsort_numeros:
-    print("\n Las listas son iguales")
-else:
-    print("\n las listas no son iguales")
+#Ordenamiento de listas incorporadas en python
+for n in tamanios:
+    numeros = [randint(1, 10000) for _ in range(n)]
+    
+    inicio = time.perf_counter()
+    numeros.sort
+    fin = time.perf_counter()
+    listasort_time.append(fin - inicio)
+
+#Grafico de los tiempos de cada tipo de ordenamiento segun tamaño de la lista 
+plt.plot(tamanios, burbuja_time, marker='o', label='Burbuja')
+plt.plot(tamanios, quicksort_time, marker='o', label='Quicksort')
+plt.plot(tamanios, radixsort_time, marker='o', label='Radix Sort')
+plt.plot(tamanios, listasort_time, marker='o', label='Sort de Python')
+
+plt.xlabel('Tamaño de la lista')
+plt.ylabel('Tiempo (segundos)')
+plt.title('Comparación de tiempos de ordenamiento')
+plt.legend() 
+plt.grid() 
+plt.show()
